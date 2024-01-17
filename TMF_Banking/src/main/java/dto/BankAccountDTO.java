4c4982050975bd39caf03a533a0eb8558aae26e9
DURGASI MAHESH KUMAR <124244425+Mahesh-100@users.noreply.github.com>
2024-01-17 13:04:11 +0530

    added dto and dao

---
+++ TMF_Banking/src/main/java/dto/BankAccountDTO.java
@@ -0,0 +1,57 @@
+package dto;
+public class BankAccountDTO {
+	 
+	private int account_id;
+	private String username;
+	private String account_no;
+	private String bank_name;
+	private String IFSC_code;
+	private String account_type;
+	private String current_balance;
+	public int getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(int account_id) {
+		this.account_id = account_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAccount_no() {
+		return account_no;
+	}
+	public void setAccount_no(String account_no) {
+		this.account_no = account_no;
+	}
+	public String getBank_name() {
+		return bank_name;
+	}
+	public void setBank_name(String bank_name) {
+		this.bank_name = bank_name;
+	}
+	public String getIFSC_code() {
+		return IFSC_code;
+	}
+	public void setIFSC_code(String iFSC_code) {
+		IFSC_code = iFSC_code;
+	}
+	public String getAccount_type() {
+		return account_type;
+	}
+	public void setAccount_type(String account_type) {
+		this.account_type = account_type;
+	}
+	public String getCurrent_balance() {
+		return current_balance;
+	}
+	public void setCurrent_balance(String current_balance) {
+		this.current_balance = current_balance;
+	}
+	
+	
+	
+	
+}
