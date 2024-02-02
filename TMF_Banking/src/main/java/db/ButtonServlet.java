a2df1f75b67d46705b36961b8a0ecbe3a2be1577
Mahesh-100 <maheshkumar.ucen@gmail.com>
2024-02-02 20:07:20 +0530

    updated

--- TMF_Banking/src/main/java/db/ButtonServlet.java
+++ TMF_Banking/src/main/java/db/ButtonServlet.java
@@ -1,7 +1,7 @@
 package db;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,18 +16,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ButtonServlet")
 public class ButtonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public ButtonServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 String selectedAction = request.getParameter("selectedAction");
 		
@@ -39,14 +33,14 @@ public class ButtonServlet extends HttpServlet {
 //		 PrintWriter out=response.getWriter();
 //		 out.append(accountNumber+""+sourceId);
 		    
-		    if ("AddMoney".equals(selectedAction)) {
+		    if ("Statement".equals(selectedAction)) {
 		        // Handle Add Money action
-		    	
-		    	RequestDispatcher rd=request.getRequestDispatcher("AddMoney.jsp");
-            	rd.forward(request, response);
-		    } else if ("Statement".equals(selectedAction)) {
-		        // Handle Request Statement action
 		    	RequestDispatcher rd=request.getRequestDispatcher("Statement.jsp");
+            	rd.forward(request, response);
+		    	
+		    } else if ("AddMoney".equals(selectedAction)) {
+		        // Handle Request Statement action
+		    	RequestDispatcher rd=request.getRequestDispatcher("AddMoney.jsp");
             	rd.forward(request, response);
 		    }else if ("SendMoney".equals(selectedAction)) {
 		        // Handle Request Statement action
@@ -54,8 +48,7 @@ public class ButtonServlet extends HttpServlet {
             	rd.forward(request, response);
 		    }
 		    else {
-		    	PrintWriter out=response.getWriter();
-		    	out.append("unable to navigate");
+		    	response.sendRedirect("Home.jsp");
 		    }
 				
 	}
